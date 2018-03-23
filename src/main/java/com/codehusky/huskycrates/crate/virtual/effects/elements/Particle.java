b2e167599d71e23521ae0673ed7e9c16f686dd05
Loki <lokio27productions@gmail.com>
2018-03-22 20:16:56 -0500

    Crate animation and color preset system, HSV support, particle repetition support

--- src/main/java/com/codehusky/huskycrates/crate/virtual/effects/elements/Particle.java
+++ src/main/java/com/codehusky/huskycrates/crate/virtual/effects/elements/Particle.java
@@ -22,6 +22,7 @@ public class Particle {
     private String animationCode;
     private Boolean animateColor;
     private CompiledScript compiled;
+    private int amount;
 
     private ArrayList<ParticleEffect> palette = new ArrayList<>();
     private ArrayList<ArrayList<Integer>> art = new ArrayList();
@@ -65,11 +66,31 @@ public class Particle {
             }
             this.particle = builder.build();
         }
+        if(node.getNode("animationPreset").isVirtual()) {
+            this.animationCode = node.getNode("animationCode").getString("x=Math.sin(time/5)*0.7;z=Math.cos(time/5)*0.7;");
+            this.animateColor = node.getNode("animateColor").getBoolean(false);
+        }else{
+            switch (node.getNode("animationPreset").getString("").toLowerCase()){
+                case "orbit":
+                    this.animationCode = "x=Math.sin(time/5)*0.7; y=Math.sin(time/3)*0.2; z=Math.cos(time/5)*0.7;";
+                    break;
+                default:
+                    throw new ConfigParseError("Invalid animation preset specified!",node.getNode("animationPreset").getPath());
+            }
+            switch (node.getNode("colorAnimationPreset").getString("").toLowerCase()){
+                case "rainbow":
+                    this.animateColor = true;
+                    this.animationCode += "; h=((time*5)%255) + 1; s=255; v=255;";
+                    break;
+                default:
+                    this.animateColor=false;
+                    break;
+            }
+        }
+        this.amount = node.getNode("amount").getInt(1);
 
-        this.animationCode = node.getNode("animationCode").getString("x=Math.sin(time/5)*0.7;z=Math.cos(time/5)*0.7;");
-        this.animateColor = node.getNode("animateColor").getBoolean(false);
         try {
-            this.compiled = ((Compilable) HuskyCrates.jsengine).compile("(function(time){var x = 0.0; var y = 0.0; var z = 0.0; var r = 0; var g = 0; var b = 0; " + animationCode + "; var result = {x:x,y:y,z:z,r:Math.round(r),g:Math.round(g),b:Math.round(b)}; return result;})(time);");
+            this.compiled = ((Compilable) HuskyCrates.jsengine).compile("function HSVtoRGB(h,s,v){var r,g,b,i,f,p,q,t;if(arguments.length===1){s=h.s,v=h.v,h=h.h}i=Math.floor(h*6);f=h*6-i;p=v*(1-s);q=v*(1-f*s);t=v*(1-(1-f)*s);switch(i%6){case 0:r=v,g=t,b=p;break;case 1:r=q,g=v,b=p;break;case 2:r=p,g=v,b=t;break;case 3:r=p,g=q,b=v;break;case 4:r=t,g=p,b=v;break;case 5:r=v,g=p,b=q;break}return{r:Math.round(r*255),g:Math.round(g*255),b:Math.round(b*255)}} (function(time, num){var x = 0.0; var y = 0.0; var z = 0.0; var h; var s; var v; var r = 0; var g = 0; var b = 0; " + animationCode + "; if(h&&s&&v){var hsv = HSVtoRGB(h/255,s/255,v/255); r=hsv.r;g=hsv.g;b=hsv.b;} var result = {x:x,y:y,z:z,r:Math.round(r),g:Math.round(g),b:Math.round(b)}; return result;})(time, num);");
         }catch(Exception e){
             e.printStackTrace();
         }
@@ -78,34 +99,34 @@ public class Particle {
 
     public void run(long tick, Location<World> location){
         try {
+            for(int i = 0; i < amount; i++) {
+                SimpleScriptContext sc = new SimpleScriptContext();
+                sc.setBindings(HuskyCrates.jsengine.createBindings(), ScriptContext.GLOBAL_SCOPE);
+                Bindings bindings = sc.getBindings(ScriptContext.GLOBAL_SCOPE);
+                bindings.put("time", tick);
+                bindings.put("num", i+1);
+                sc.setBindings(bindings, ScriptContext.GLOBAL_SCOPE);
+                ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) compiled.eval(sc);
 
-            SimpleScriptContext sc = new SimpleScriptContext();
-            sc.setBindings(HuskyCrates.jsengine.createBindings(), ScriptContext.GLOBAL_SCOPE);
-            Bindings bindings = sc.getBindings(ScriptContext.GLOBAL_SCOPE);
-            bindings.put("time",tick);
-            sc.setBindings(bindings,ScriptContext.GLOBAL_SCOPE);
-            ScriptObjectMirror scriptObjectMirror = (ScriptObjectMirror) compiled.eval(sc);
+                double x = (double) scriptObjectMirror.get("x");
+                double y = (double) scriptObjectMirror.get("y");
+                double z = (double) scriptObjectMirror.get("z");
 
-            double x = (double) scriptObjectMirror.get("x");
-            double y = (double) scriptObjectMirror.get("y");
-            double z = (double) scriptObjectMirror.get("z");
+                int r = Math.max(0, Math.min(255, ((Double) scriptObjectMirror.get("r")).intValue()));
+                int g = Math.max(0, Math.min(255, ((Double) scriptObjectMirror.get("g")).intValue()));
+                int b = Math.max(0, Math.min(255, ((Double) scriptObjectMirror.get("b")).intValue()));
 
-            int r = Math.max(0,Math.min(255,((Double) scriptObjectMirror.get("r")).intValue()));
-            int g = Math.max(0,Math.min(255,((Double) scriptObjectMirror.get("g")).intValue()));
-            int b = Math.max(0,Math.min(255,((Double) scriptObjectMirror.get("b")).intValue()));
+                ParticleEffect animatedParticle = null;
 
-            System.out.println(r + "," + g + "," + b);
+                if (this.animateColor) {
+                    animatedParticle = ParticleEffect.builder()
+                            .from(particle)
+                            .option(ParticleOptions.COLOR, Color.ofRgb(r, g, b))
+                            .build();
+                }
 
-            ParticleEffect animatedParticle = null;
-
-            if(this.animateColor) {
-                animatedParticle = ParticleEffect.builder()
-                        .from(particle)
-                        .option(ParticleOptions.COLOR,Color.ofRgb(r,g,b))
-                        .build();
+                location.getExtent().spawnParticles((this.animateColor) ? animatedParticle : particle, location.getPosition().clone().add(0.5, 0.5, 0.5).add(x, y, z));
             }
-
-            location.getExtent().spawnParticles((this.animateColor)?animatedParticle:particle,location.getPosition().clone().add(0.5,0.5,0.5).add(x,y,z));
         } catch (ScriptException e) {
             e.printStackTrace();
         }
