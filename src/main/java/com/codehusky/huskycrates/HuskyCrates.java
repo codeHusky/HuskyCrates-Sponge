d7fa463245d15e883d0561918fd60c5d64977a45
Loki <lokio27productions@gmail.com>
2018-04-22 18:21:33 -0500

    bump to HUI 0.6.0PRE2

--- src/main/java/com/codehusky/huskycrates/HuskyCrates.java
+++ src/main/java/com/codehusky/huskycrates/HuskyCrates.java
@@ -43,7 +43,7 @@ import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 
-@Plugin(id="huskycrates", name = "HuskyCrates", version = "2.0.0PRE7", description = "A Crate Plugin for Sponge!",dependencies = {@Dependency(id="huskyui",version = "0.5.2")})
+@Plugin(id="huskycrates", name = "HuskyCrates", version = "2.0.0PRE7", description = "A Crate Plugin for Sponge!",dependencies = {@Dependency(id="huskyui",version = "0.6.0PRE2")})
 public class HuskyCrates {
     //@Inject
     public Logger logger;
